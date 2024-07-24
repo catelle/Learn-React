a13fd1f78248416320b68f628050d955ae4825a4
catelle <catelleningha@gmail.com>
2024-07-24 16:50:21 +0100

    how to set variable set after an event

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const User = (props) => {
+
+const [click, setClick]= useState(false);
+
+const clickBut= ()=>{
+    setClick(true)
+}
+
   return (
     <div><h1>User:</h1>
         <h2>Username: {props.username}</h2>
         <h2>Userage: {props.age}</h2>
+        {!click && <h3>The user didn't clic on button</h3>} 
+        {click &&<h3>The user clicked on the button</h3>}
+        <button onClick={clickBut}>click</button>
+
     </div>
   )
 }
